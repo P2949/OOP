5c2bf86c4dabd8d0637b7ec148e9152c702aea2c
p2949 <24377619@studentmail.ul.ie>
2025-11-05 14:57:05 +0000

    group class for lab a tut groups

---
+++ OOP/src/Group.java
@@ -0,0 +1,11 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Group {
+
+    private char groupID;
+    private Module module;
+    private List<Student> students = new ArrayList<>();
+
+}
--- OOP/src/Student.java
+++ OOP/src/Student.java
@@ -1,11 +1,13 @@
 import java.time.LocalDate;
 
+import java.util.List;
+
 public class Student extends Person {
 
     private int studentID;
     private Program program;
     private int yearOfStudy;
-    private char labGroup;
+    private List<Group> labGroups;
 
     public Student(String name, int age, String studentID, Program program, int yearOfStudy) {
         super(name, age);
@@ -36,13 +38,5 @@ public class Student extends Person {
 
     public void setYearOfStudy(int yearOfStudy) {
         this.yearOfStudy = yearOfStudy;
-    }
-
-    public char getLabGroup() {
-        return labGroup;
-    }
-
-    public void setLabGroup(char labGroup) {
-        this.labGroup = labGroup;
     }
 }
