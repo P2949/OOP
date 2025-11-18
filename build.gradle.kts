2a9a4cf28404f524efb1c65d7115583cbb9a4f7f
p2949 <24377619@studentmail.ul.ie>
2025-11-18 17:52:27 +0000

    Used a bit of the opencsv library just as testing.
    gradle is very useful and simple to use, opencsv seems like a good library to use for our persistent files.
    
    I'll be merging into main as soon as I get permission from the rest of the team.

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,9 +7,12 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    google()
 }
 
 dependencies {
+    implementation("com.opencsv:opencsv:5.12.0")
+    implementation("org.jetbrains:annotations:26.0.2-1")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
